782cffe1cfd3d3a147827090f645646028280f33
Oonishi228 <oasttsv8@gmail.com>
2022-10-14 14:02:19 +0900

    switch文をswitch式に変更。改行などの修正をしました。

--- src/main/java/com/example/HelloWorldApplication/FormController.java
+++ src/main/java/com/example/HelloWorldApplication/FormController.java
@@ -2,7 +2,9 @@ package com.example.HelloWorldApplication;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -15,29 +17,27 @@ public class FormController {
         model.addAttribute("moji", "Hello World!");
         //指定された名前に指定された属性を追加する。
 
-        String now = LocalDateTime.now().format( DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH時mm分ss秒 E曜日")); // ②
-        model.addAttribute( "datetime", now );
+        String now =
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH時mm分ss秒 E曜日"));
+        model.addAttribute("datetime", now);
 
         return "form";
     }
 
     @PostMapping("/greeting")
     public String result(@RequestParam String countryName, Model model) {
-        String countryGreeting = "こんにちは！";
-        switch (countryName) {
-            case "japan":
-                countryGreeting = "こんにちは！";
-                break;
-            case "us":
-                countryGreeting = "Hello！";
-                break;
-            case "korean":
-                countryGreeting = "안녕하세요！";
-                break;
-            case "china":
-                countryGreeting = "你好！";
-                break;
-        }
+        String countryGreeting = switch (countryName) {
+            case "japan" -> "こんにちは！";
+
+            case "us" -> "Hello！";
+
+            case "korean" -> "안녕하세요！";
+
+            case "china" -> "你好！";
+
+            default -> "エラーです。";
+
+        };
         model.addAttribute("countryGreeting", countryGreeting);
         return "greeting";
     }
